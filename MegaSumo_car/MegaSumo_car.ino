4ab93b7458fcca180813a639921af0e42e49baf2
ahmed-ibrahim-asl <aassal950@gmail.com>
2024-09-20 06:54:33 +0300

    1.0.0

--- MegaSumo_car/MegaSumo_car.ino
+++ MegaSumo_car/MegaSumo_car.ino
@@ -1,164 +1,208 @@
-#define FRONT_TRIG_PIN 2
-#define FRONT_ECHO_PIN 3
-#define BACK_TRIG_PIN 8
-#define BACK_ECHO_PIN 9
-#define LEFT_TRIG_PIN 10
-#define LEFT_ECHO_PIN 11
-#define RIGHT_TRIG_PIN 6
-#define RIGHT_ECHO_PIN 7
+/*************************************** Hardware Pin Mapping ***************************************/
+#define FRONT_ULTRASONIC_TRIG_PIN                                  2
+#define FRONT_ULTRASONIC_ECHO_PIN                                  3
 
-#define MOTOR1_PIN1 A0
-#define MOTOR1_PIN2 A1
-#define MOTOR2_PIN1 A2
-#define MOTOR2_PIN2 A3
+#define BACK_ULTRASONIC_TRIG_PIN                                   8
+#define BACK_ULTRASONIC_ECHO_PIN                                   9
 
-#define IR1 4
-#define IR2 5
-#define IR3 A4
-#define IR4 A5
+#define RIGHT_ULTRASONIC_TRIG_PIN                                  6
+#define RIGHT_ULTRASONIC_ECHO_PIN                                  7
 
-#define MAX_DISTANCE 30
+#define LEFT_ULTRASONIC_TRIG_PIN                                   10
+#define LEFT_ULTRASONIC_ECHO_PIN                                   11
+
+#define MOTOR_LEFT_FORWARD_PIN                                     A0
+#define MOTOR_LEFT_BACKWARD_PIN                                    A1
+
+#define MOTOR_RIGHT_FORWARD_PIN                                    A2
+#define MOTOR_RIGHT_BACKWARD_PIN                                   A3
+
+#define IR_FRONT_RIGHT_PIN                                         4
+#define IR_FRONT_LEFT_PIN                                          5
+
+#define IR_BACK_RIGHT_PIN                                          A5
+#define IR_BACK_LEFT_PIN                                           A4
+/****************************************************************************************************/
+
+/********************************** Global Configuration Settings ***********************************/
+#define ULTRASONIC_MAX_DISTANCE                                    25
+#define BAUD_RATE                                                  9600
+#define STARTUP_DELAY_MS                                           5000
+#define TURN_DELAY_MS                                              150
+#define MOVE_DELAY_MS                                              1000
+#define STOP_DELAY_MS                                              1000
+/****************************************************************************************************/
+
+/************************************** Sensor Reading Macros **************************************/
+#define GetDistance_FRONT_ULTRASONIC()            getDistance(FRONT_ULTRASONIC_TRIG_PIN, FRONT_ULTRASONIC_ECHO_PIN)
+#define GetDistance_BACK_ULTRASONIC()             getDistance(BACK_ULTRASONIC_TRIG_PIN, BACK_ULTRASONIC_ECHO_PIN)
+
+#define GetDistance_RIGHT_ULTRASONIC()            getDistance(RIGHT_ULTRASONIC_TRIG_PIN, RIGHT_ULTRASONIC_ECHO_PIN)
+#define GetDistance_LEFT_ULTRASONIC()             getDistance(LEFT_ULTRASONIC_TRIG_PIN, LEFT_ULTRASONIC_ECHO_PIN)
+
+#define GetReading_FRONT_RIGHT_IR()               digitalRead(IR_FRONT_RIGHT_PIN)
+#define GetReading_FRONT_LEFT_IR()                digitalRead(IR_FRONT_LEFT_PIN)
+
+#define GetReading_BACK_RIGHT_IR()                digitalRead(IR_BACK_RIGHT_PIN)
+#define GetReading_BACK_LEFT_IR()                 digitalRead(IR_BACK_LEFT_PIN)
+/****************************************************************************************************/
+
+/********************* Functions Prototypes **********************/
+inline int getDistance(byte trig_pin, byte echo_pin);
+/*****************************************************************/
+
+/********************* Global Variables **************************/
+int frontDist, backDist, leftDist, rightDist;
+/*****************************************************************/
 
 void setup() {
-  pinMode(FRONT_TRIG_PIN, OUTPUT);
-  pinMode(FRONT_ECHO_PIN, INPUT);
-  pinMode(BACK_TRIG_PIN, OUTPUT);
-  pinMode(BACK_ECHO_PIN, INPUT);
-  pinMode(LEFT_TRIG_PIN, OUTPUT);
-  pinMode(LEFT_ECHO_PIN, INPUT);
-  pinMode(RIGHT_TRIG_PIN, OUTPUT);
-  pinMode(RIGHT_ECHO_PIN, INPUT);
-  pinMode(MOTOR1_PIN1, OUTPUT);
-  pinMode(MOTOR1_PIN2, OUTPUT);
-  pinMode(MOTOR2_PIN1, OUTPUT);
-  pinMode(MOTOR2_PIN2, OUTPUT);
 
-  delay(5000);
-  Serial.begin(9600);
+  /*********************** Front UltraSonic ***********************/
+  pinMode(FRONT_ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(FRONT_ULTRASONIC_ECHO_PIN, INPUT);
+  /****************************************************************/
+
+  /************************ Back UltraSonic ************************/
+  pinMode(BACK_ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(BACK_ULTRASONIC_ECHO_PIN, INPUT);
+  /*****************************************************************/
+
+  /************************ Left UltraSonic ************************/
+  pinMode(LEFT_ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(LEFT_ULTRASONIC_ECHO_PIN, INPUT);
+  /*****************************************************************/
+
+  /************************ Right UltraSonic ***********************/
+  pinMode(RIGHT_ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(RIGHT_ULTRASONIC_ECHO_PIN, INPUT);
+  /*****************************************************************/
+
+  /*************************** DC Motors ***************************/
+  pinMode(MOTOR_LEFT_FORWARD_PIN  , OUTPUT);
+  pinMode(MOTOR_LEFT_BACKWARD_PIN , OUTPUT);
+  pinMode(MOTOR_RIGHT_FORWARD_PIN , OUTPUT);
+  pinMode(MOTOR_RIGHT_BACKWARD_PIN, OUTPUT);
+  /*****************************************************************/
+
+  delay(STARTUP_DELAY_MS);
+  Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-  int frontDist = getDistance(FRONT_TRIG_PIN, FRONT_ECHO_PIN);
-  int backDist = getDistance(BACK_TRIG_PIN, BACK_ECHO_PIN);
-  int leftDist = getDistance(LEFT_TRIG_PIN, LEFT_ECHO_PIN);
-  int rightDist = getDistance(RIGHT_TRIG_PIN, RIGHT_ECHO_PIN);
-  int irFrontLeft = digitalRead(IR4);
-  int irFrontRight = digitalRead(IR2);
-  int irBackLeft = digitalRead(IR3);
-  int irBackRight = digitalRead(IR1);
 
-  if (irFrontLeft == 1 || irFrontRight == 0)  {
-    // If front IR sensors detect white
-    digitalWrite(MOTOR1_PIN1, 1);
-    digitalWrite(MOTOR1_PIN2, 0);
-    digitalWrite(MOTOR2_PIN1, 1);
-    digitalWrite(MOTOR2_PIN2, 0);
-    delay(1000);
+  // Capture ultrasonic distances at the start of the loop for efficient decision-making
+  frontDist = GetDistance_FRONT_ULTRASONIC();
+  backDist = GetDistance_BACK_ULTRASONIC();
+  leftDist = GetDistance_LEFT_ULTRASONIC();
+  rightDist = GetDistance_RIGHT_ULTRASONIC();
 
-  } else if (irBackLeft == 0 && irBackRight == 0) {
-    // If back IR sensors detect white
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 1);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 1);
-    delay(1000);
+  // React fast to IR sensor readings (real-time boundary detection)
+  if (GetReading_FRONT_LEFT_IR() == 1 || GetReading_FRONT_RIGHT_IR() == 0) {
+    // Front IR triggered: Move aggressively forward
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 1);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
+    delay(MOVE_DELAY_MS);
 
-  } else if (irBackLeft == 0 || irBackRight == 0) {
-    // If any back IR sensor detects white
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 0);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 0);
-    delay(1000);
+  } else if (GetReading_BACK_LEFT_IR() == 0 && GetReading_BACK_RIGHT_IR() == 0) {
+    // Both back IR triggered: Retreat (possibly near arena boundary)
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+    delay(MOVE_DELAY_MS);
 
-  } else if (irBackRight == 0 && irFrontRight == 0) {
-    // If right IR sensors detect white
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 1);
-    digitalWrite(MOTOR2_PIN1, 1);
-    digitalWrite(MOTOR2_PIN2, 0);
-    delay(100);
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 1);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 1);
-    delay(500);
-    
-  } else if (irFrontLeft == 1 && irBackLeft == 0) {
-    // If left IR sensors detect white
-    digitalWrite(MOTOR1_PIN1, 1);
-    digitalWrite(MOTOR1_PIN2, 0);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 1);
-    delay(100);
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 1);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 1);
-    delay(500);
-    
-  } else if (irBackLeft == 0 && irBackRight == 0 && irFrontLeft == 1 && irBackLeft == 0 ) {
-    // Stop if all IR sensors detect white
-    digitalWrite(MOTOR1_PIN1, 0);
-    digitalWrite(MOTOR1_PIN2, 0);
-    digitalWrite(MOTOR2_PIN1, 0);
-    digitalWrite(MOTOR2_PIN2, 0);
+  } else if (GetReading_BACK_LEFT_IR() == 0 || GetReading_BACK_RIGHT_IR() == 0) {
+    // One back IR triggered: Stop immediately to avoid falling out
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
+    delay(STOP_DELAY_MS);
+
+  } else if (GetReading_BACK_RIGHT_IR() == 0 && GetReading_FRONT_RIGHT_IR() == 0) {
+    // Right side detected: Quick turn to reposition
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
+    delay(TURN_DELAY_MS);
+    // Additional turn for full repositioning
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+    delay(MOVE_DELAY_MS);
+
+  } else if (GetReading_FRONT_LEFT_IR() == 1 && GetReading_BACK_LEFT_IR() == 0) {
+    // Left side detected: Turn left to face opponent
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 1);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+    delay(TURN_DELAY_MS);
+    // Final adjustment
+    digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+    digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+    delay(MOVE_DELAY_MS);
 
   } else {
-    // Default ultrasonic-based obstacle avoidance
-    if (frontDist <= MAX_DISTANCE || backDist <= MAX_DISTANCE || leftDist <= MAX_DISTANCE || rightDist <= MAX_DISTANCE) {
+    // Ultrasonic sensor-based opponent avoidance
+    if (frontDist <= ULTRASONIC_MAX_DISTANCE || 
+        backDist <= ULTRASONIC_MAX_DISTANCE || 
+        leftDist <= ULTRASONIC_MAX_DISTANCE || 
+        rightDist <= ULTRASONIC_MAX_DISTANCE) {
+      
       if (frontDist <= backDist && frontDist <= leftDist && frontDist <= rightDist) {
-        // Move forward
-        digitalWrite(MOTOR1_PIN1, 0);
-        digitalWrite(MOTOR1_PIN2, 1);
-        digitalWrite(MOTOR2_PIN1, 0);
-        digitalWrite(MOTOR2_PIN2, 1);
-        
+        // Opponent detected in front: Charge forward
+        digitalWrite(MOTOR_LEFT_FORWARD_PIN, 1);
+        digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+        digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 1);
+        digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
+
       } else if (backDist <= leftDist && backDist <= rightDist) {
-        // Move backward
-        digitalWrite(MOTOR1_PIN1, 1);
-        digitalWrite(MOTOR1_PIN2, 0);
-        digitalWrite(MOTOR2_PIN1, 1);
-        digitalWrite(MOTOR2_PIN2, 0);
-        
+        // Opponent behind: Retreat and reposition
+        digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+        digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+        digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+        digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+
       } else if (leftDist <= rightDist) {
-        // Turn left
-        digitalWrite(MOTOR1_PIN1, 0);
-        digitalWrite(MOTOR1_PIN2, 1);
-        digitalWrite(MOTOR2_PIN1, 1);
-        digitalWrite(MOTOR2_PIN2, 0);
-        delay(150);
-        
+        // Opponent on left: Turn left
+        digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+        digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 1);
+        digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 1);
+        digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
+        delay(TURN_DELAY_MS);
+
       } else if (rightDist <= leftDist) {
-        // Turn right
-        digitalWrite(MOTOR1_PIN1, 1);
-        digitalWrite(MOTOR1_PIN2, 0);
-        digitalWrite(MOTOR2_PIN1, 0);
-        digitalWrite(MOTOR2_PIN2, 1);
-        delay(150);
+        // Opponent on right: Turn right
+        digitalWrite(MOTOR_LEFT_FORWARD_PIN, 1);
+        digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+        digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+        digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 1);
+        delay(TURN_DELAY_MS);
       }
     } else {
-      // Stop if no obstacles detected
-      digitalWrite(MOTOR1_PIN1, 0);
-      digitalWrite(MOTOR1_PIN2, 0);
-      digitalWrite(MOTOR2_PIN1, 0);
-      digitalWrite(MOTOR2_PIN2, 0);
+      // No opponent or boundary detected: Stop
+      digitalWrite(MOTOR_LEFT_FORWARD_PIN, 0);
+      digitalWrite(MOTOR_LEFT_BACKWARD_PIN, 0);
+      digitalWrite(MOTOR_RIGHT_FORWARD_PIN, 0);
+      digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, 0);
     }
   }
 }
 
-int getDistance(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW);
+inline int getDistance(byte trig_pin, byte echo_pin) {
+  digitalWrite(trig_pin, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trig_pin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  long duration = pulseIn(echoPin, HIGH);
-  int distance = duration * 0.034 / 2;
-  Serial.print("Sensor ");
-  Serial.print(trigPin);
-  Serial.print(" reading: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  return distance;
+  digitalWrite(trig_pin, LOW);
+
+  long duration = pulseIn(echo_pin, HIGH);
+  return duration * 0.034 / 2;
 }
